2ba4a188043a9870dd1a09e73eaec167e849a4bd
theBowja <ericxin@gmail.com>
2018-05-21 18:17:04 -0400

    changed config files

--- config/config.js
+++ config/config.js
@@ -11,8 +11,8 @@ config.secret = 'certifiednoideawhatimdoing';
 
 config.db = {};
 config.db.hostname = process.env.RDS_HOSTNAME || 'localhost';
-config.db.username = process.env.RDS_USERNAME || 'me';
-config.db.password = process.env.RDS_PASSWORD || 'mario';
+config.db.username = process.env.RDS_USERNAME || 'meme';
+config.db.password = process.env.RDS_PASSWORD || 'mariomario';
 config.db.port 	   = process.env.RDS_PORT     || 3306;
 config.db.dbname   = process.env.RDS_DB_NAME  || 'distincttest';
 
