65e4cd778703e6ab123250b15f6eea9b0457b98f
theBowja <ericxin@gmail.com>
2018-07-03 10:00:42 -0400

    updated README

--- config/config.js
+++ config/config.js
@@ -10,8 +10,8 @@ config.secret = 'certifiednoideawhatimdoing';
 
 
 config.db = {};
-config.db.hostname 	= process.env.RDS_HOSTNAME || 'localhost';
-config.db.username 	= process.env.RDS_USERNAME || 'me';
+config.db.hostname 	= process.env.RDS_HOSTNAME || 'localhost'; 
+config.db.username 	= process.env.RDS_USERNAME || 'me'; // edit the part after the '||'
 config.db.password 	= process.env.RDS_PASSWORD || 'mario';
 config.db.port 	   	= process.env.RDS_PORT     || 3306;
 config.db.dbname   	= process.env.RDS_DB_NAME  || 'distincttest';
