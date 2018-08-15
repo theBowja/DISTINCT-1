5ad690bf2c0d39788bdce67a4b25bf4a3d94c858
theBowja <ericxin@gmail.com>
2018-08-15 10:56:27 -0400

    minor text fixes

--- config/config.js
+++ config/config.js
@@ -12,9 +12,9 @@ config.secret = 'certifiednoideawhatimdoing';
 config.db = {};
 config.db.hostname 	= process.env.RDS_HOSTNAME || 'localhost'; 
 config.db.username 	= process.env.RDS_USERNAME || 'me'; // edit the part after the '||'
-config.db.password 	= process.env.RDS_PASSWORD || 'mario';
+config.db.password 	= process.env.RDS_PASSWORD || 'mario'; // edit the part after the '||'
 config.db.port 	   	= process.env.RDS_PORT     || 3306;
-config.db.dbname   	= process.env.RDS_DB_NAME  || 'distincttest';
+config.db.dbname   	= process.env.RDS_DB_NAME  || 'distincttest'; // edit the part after the '||'
 
 config.filedirectory = process.env.MOUNT_DIRECTORY || './public/uploads';
 
