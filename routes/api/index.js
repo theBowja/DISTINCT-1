5da8a81f787d5388d53fa4cfd1a98e58a83570c6
theBowja <ericxin@gmail.com>
2018-09-10 23:07:39 -0400

    removed java package

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ var api = require('express').Router();
 
 
 var topo = require('./topo.js');
-var ahab = require('./ahab.js');
+//var ahab = require('./ahab.js');
 var rsvn = require('./reservation.js');
 var reso = require('./resource.js');
 var slic = require('./slice.js');
@@ -14,7 +14,7 @@ api.use('/', rsvn);
 api.use('/', reso);	
 api.use('/', slic);
 
-api.use('/ahab', ahab);
+// api.use('/ahab', ahab);
 // due to middleware in ahab.js, there should be no paths after this.
 
 module.exports = api;
